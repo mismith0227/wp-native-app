63477dd282b272c7b43ed5b084e6d75d783fd1d4
mismith0227 <doraepon2216@gmail.com>
2017-08-14 22:09:50 +0900

    add stack nav

---
+++ component/Detail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {
+  ScrollView,
+  WebView,
+  Text,
+  View
+} from 'react-native'
+import { Header, Body, Left, Right, Title, Button, Icon, Container, Content, Spinner } from 'native-base'
+
+export default class ArticleDetail extends Component {
+  render () {
+    return (
+      <Container>
+        <Text>detail </Text>
+      </Container>
+    )
+  }
+}
--- component/PageA.js
+++ component/PageA.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {
   View,
+  ScrollView,
   Text,
   TouchableOpacity,
   StyleSheet
 } from 'react-native'
 import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base'
+import { StackNavigator } from 'react-navigation'
+import ArticleDetail from './Detail'
 
 class PageA extends React.Component {
+  onLinkDetail (text) {
+    this.props.onLinkDetail(text)
+  }
   render () {
     return (
       <Container>
@@ -26,13 +32,38 @@ class PageA extends React.Component {
             </Button>
           </Right>
         </Header>
+        <Button
+          onPress={this.onLinkDetail.bind(this)}
+        >
+          <Text>Click Me! </Text>
+        </Button>
       </Container>
     )
   }
 }
 
-const PageAScreen = ({ navigation }) => (
-  <PageA banner='Home Tab' navigation={navigation} />
+const PageAboard = ({ navigation }) => (
+  <PageA
+    onLinkDetail={(text) => navigation.navigate('Detail')}
+  />
 )
+
+const MyProfileScreen = ({ navigation }) => (
+  <ArticleDetail
+    navigation={navigation}
+  />
+)
+
+const PageAScreen = StackNavigator({
+  Root: {
+    screen: PageAboard
+  },
+  Detail: {
+    screen: MyProfileScreen,
+    navigationOptions: {
+      title: 'Detail'
+    }
+  }
+}, {headerMode: 'none'})
 
 export default PageAScreen
